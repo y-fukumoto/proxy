ce3c65cffe4386bf88fe7d41ff00a44c90e5c8f4
y-fukumoto <helmutlang1028@gmail.com>
2023-09-21 19:32:15 +0900

    no message

---
+++ start.sh
@@ -0,0 +1,10 @@
+yum -y install squid
+curl -o etc/squid/squid.conf https://raw.githubusercontent.com/y-fukumoto/proxy/main/squid..conf
+IP_ADDRESS = $1
+PORT_NUMBER = $1
+sed -i s/access_source/$IP_ADDRESS/ etc/squid/squid.conf
+sed -i s/3128/$PORT_NUMBER/ etc/squid/squid.conf
+systemctl start squid
+systemctl start firewalld
+firewall-cmd --zone=public --add-port=${port}/tcp --permanent
+firewall-cmd --reload
